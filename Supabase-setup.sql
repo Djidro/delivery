8db2177c6b30ee14ce87dab5a04a2cd8a3c89823
nadjib <Nadjibharera@gmail.com>
2026-01-01 09:07:53 +0400

    firebase-config.js

--- Supabase-setup.sql
+++ Supabase-setup.sql
@@ -1,4 +1,4 @@
-// firebase-config.js
+ // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
